0ec8697488a8c52f4f2928e9b7843be6bf37e296
Clément BRIAND <clement.briand@etudiant.univ-rennes1.fr>
2021-11-21 14:34:37 +0100

    🏗️ changed bank signatures

--- spring-boot-sample-simple-standalone/src/main/java/sample/simple/bank/IBank.java
+++ spring-boot-sample-simple-standalone/src/main/java/sample/simple/bank/IBank.java
@@ -1,5 +1,11 @@
 package sample.simple.bank;
 
 public interface IBank {
-    void transfert();
+    /**
+     * Transfert money from an account to another
+     * @param amount the amount to be transferred
+     * @param emitterId the id of the source account
+     * @param receiverId the id of the receiver account
+     */
+    void transfert(double amount, int emitterId, int receiverId);
 }
--- spring-boot-sample-simple-standalone/src/main/java/sample/simple/bank/impl/Bank.java
+++ spring-boot-sample-simple-standalone/src/main/java/sample/simple/bank/impl/Bank.java
@@ -3,10 +3,16 @@ package sample.simple.bank.impl;
 import org.springframework.stereotype.Component;
 import sample.simple.bank.IBank;
 
+import java.util.logging.Logger;
+
 @Component
 public class Bank implements IBank {
+    /**
+     * {@inheritDoc}
+     * Logs the transfert into Global logger
+     */
     @Override
-    public void transfert() {
-
+    public void transfert(double amount, int emitterId, int receiverId) {
+        Logger.getGlobal().info(String.format("[Bank] Tranfered %f from %d to %d", amount, emitterId, receiverId));
     }
 }
