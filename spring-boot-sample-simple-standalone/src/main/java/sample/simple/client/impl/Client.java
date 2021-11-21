40ab032d5d199a81a81182f42fd3e09c188974ba
Clément BRIAND <clement.briand@etudiant.univ-rennes1.fr>
2021-11-21 17:23:29 +0100

    ▶ scenario is working!

--- spring-boot-sample-simple-standalone/src/main/java/sample/simple/client/impl/Client.java
+++ spring-boot-sample-simple-standalone/src/main/java/sample/simple/client/impl/Client.java
@@ -1,14 +1,66 @@
 package sample.simple.client.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import sample.simple.client.IClient;
+import sample.simple.domain.Article;
+import sample.simple.provider.IProvider;
+import sample.simple.store.IStore;
+import sample.simple.store.impl.Store;
 
+@Component
 public class Client implements IClient {
+
+    @Autowired
+    Store store;
+
+    @Autowired
+    IProvider provider;
+
     /**
      * {@inheritDoc}
      * Scenario of program usage
      */
     @Override
     public void run() {
+
+        Article screen = new Article(1, 200.0, 15, "Screen");
+        Article desktop = new Article(2, 123.4, 10, "Desktop");
+        Article chair = new Article(3, 430, 50, "Chair");
+
+        // add articles to the provider
+        provider.addArticle(screen);
+        provider.addArticle(desktop);
+        provider.addArticle(chair);
+
+        // add articles to the store
+        store.addArticle(screen);
+        store.addArticle(desktop);
+
+        // get price for a screen
+        System.out.println(store.getPrice(1));
+
+
+        // show screen information from the store:
+        System.out.println(screen);
+
+        //order a screen
+        store.oneShotOrder(1, 1, 123);
+
+        // show screen information from the store:
+        System.out.println(screen);
+
+        // buy two chairs as an oneshotorder
+        store.oneShotOrder(3, 2, 123);
+
+        // add a desktop to the cart
+        store.addItemToCart(2, 1, 1);
+
+        // pay for the cart
+        store.pay(1, 123);
+
+        // show desktop info
+        System.out.println(desktop);
 
     }
 }
