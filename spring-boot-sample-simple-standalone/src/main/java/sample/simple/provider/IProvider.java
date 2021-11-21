b99f0f02bd41608337b7ae62ed62df29f01d048e
Clément BRIAND <clement.briand@etudiant.univ-rennes1.fr>
2021-11-21 14:56:30 +0100

    🏗️ changed provider signatures 📝 added javadoc

--- spring-boot-sample-simple-standalone/src/main/java/sample/simple/provider/IProvider.java
+++ spring-boot-sample-simple-standalone/src/main/java/sample/simple/provider/IProvider.java
@@ -1,6 +1,17 @@
 package sample.simple.provider;
 
 public interface IProvider {
-    double getPrice();
-    void order(Object article);
+    /**
+     * Getter for the article price
+     * @param articleId the id of the wanted article
+     * @return the article price
+     */
+    double getPrice(int articleId);
+
+    /**
+     * Order the article designed by it id in given quantity
+     * @param articleId the article we want to order
+     * @param quantity the quantity in which we want to order the article
+     */
+    void order(int articleId, int quantity);
 }
--- spring-boot-sample-simple-standalone/src/main/java/sample/simple/provider/impl/Provider.java
+++ spring-boot-sample-simple-standalone/src/main/java/sample/simple/provider/impl/Provider.java
@@ -3,15 +3,30 @@ package sample.simple.provider.impl;
 import org.springframework.stereotype.Component;
 import sample.simple.provider.IProvider;
 
+import java.util.logging.Logger;
+
 @Component
 public class Provider implements IProvider {
+    /**
+     * {@inheritDoc}
+     * @param articleId the id of the wanted article
+     * @return the price
+     * Logs into global the result
+     */
     @Override
-    public double getPrice() {
+    public double getPrice(int articleId) {
+        Logger.getGlobal().info(String.format("[Provider] Got the price for %d", articleId));
         return 0;
     }
 
+    /**
+     * {@inheritDoc}
+     * @param articleId the article we want to order
+     * @param quantity the quantity in which we want to order the article
+     * Logs into global the result
+     */
     @Override
-    public void order(Object article) {
-
+    public void order(int articleId, int quantity) {
+        Logger.getGlobal().info(String.format("[Provider] Ordered %d of %d", quantity, articleId));
     }
 }
