69817902d6b0f2ff2c2e28bea5709b52ce664ed7
Clément BRIAND <clement.briand@etudiant.univ-rennes1.fr>
2021-11-22 17:32:20 +0100

    🔊 added logs by aspectj

--- spring-boot-sample-aop-standalone/src/main/java/sample/aop/monitor/ServiceMonitor.java
+++ spring-boot-sample-aop-standalone/src/main/java/sample/aop/monitor/ServiceMonitor.java
@@ -17,18 +17,62 @@
 package sample.aop.monitor;
 
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 
+import org.aspectj.lang.annotation.Pointcut;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
 
 @Aspect
 @Component
 public class ServiceMonitor {
 
-	@AfterReturning("execution(* sample..*Service.*(..))")
-	public void logServiceAccess(JoinPoint joinPoint) {
-		System.out.println("Completed: " + joinPoint);
-	}
+    /**
+     * Creates an appropriate logger depending on the joinPoint
+     *
+     * @param joinPoint the joinPoint for which we need a logger
+     * @return the logger
+     */
+    private Logger getLogger(JoinPoint joinPoint) {
+        return LoggerFactory.getLogger(joinPoint.getSignature().getDeclaringTypeName());
+    }
+
+    /**
+     *
+     */
+    @Pointcut("within(sample.aop..*)")
+    public void applicationPointCut() {
+        // Method is empty as this is just a Pointcut, the implementations are in the method annotated @Around.
+    }
+
+    /**
+     * Log when we enter and exit a method
+     *
+     * @param joinPoint
+     * @return
+     * @throws Throwable
+     */
+    @Around("applicationPointCut()")
+    public Object log(ProceedingJoinPoint joinPoint) throws Throwable {
+        Logger log = getLogger(joinPoint);
+		if (log.isDebugEnabled())
+			log.debug("Enter: {}() with argument[s] = {}", joinPoint.getSignature().getName(), Arrays.toString(joinPoint.getArgs()));
+        try {
+            Object result = joinPoint.proceed();
+			if (log.isDebugEnabled())
+            	log.debug("Exit: {}() with result = {}", joinPoint.getSignature().getName(), result);
+
+            return result;
+        } catch (IllegalArgumentException e) {
+            log.error("Illegal argument: {} in {}()", Arrays.toString(joinPoint.getArgs()), joinPoint.getSignature().getName());
+            throw e;
+        }
+    }
 
 }
