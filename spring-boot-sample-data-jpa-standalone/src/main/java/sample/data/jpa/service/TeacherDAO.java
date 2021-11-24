9b7f49f8fcf7bd24042c83653446ce84d4cbf26c
Clément BRIAND <clement.briand@etudiant.univ-rennes1.fr>
2021-11-24 11:11:22 +0100

    🧐 init teacher controller

--- spring-boot-sample-data-jpa-standalone/src/main/java/sample/data/jpa/service/TeacherDAO.java
+++ spring-boot-sample-data-jpa-standalone/src/main/java/sample/data/jpa/service/TeacherDAO.java
@@ -3,4 +3,8 @@ package sample.data.jpa.service;
 import org.springframework.data.jpa.repository.JpaRepository;
 import sample.data.jpa.domain.Teacher;
 
-public interface TeacherDAO extends JpaRepository<Teacher, Long> {}
+import java.util.Optional;
+
+public interface TeacherDAO extends JpaRepository<Teacher, Long> {
+    Optional<Teacher> findTeacherByFirstNameAndLastName(String firstName, String lastName);
+}
